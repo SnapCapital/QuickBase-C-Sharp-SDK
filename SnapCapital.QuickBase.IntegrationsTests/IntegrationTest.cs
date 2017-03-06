4aa8e0d7ed796fb1a40f3e7be0a339cea099a6f8
Uri Goldstein <uri.goldstein@gmail.com>
2017-03-06 14:50:07 +0200

    Switched integ tests to NUnit

---
+++ SnapCapital.QuickBase.IntegrationsTests/IntegrationTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SnapCapital.QuickBase.IntegrationsTests
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
