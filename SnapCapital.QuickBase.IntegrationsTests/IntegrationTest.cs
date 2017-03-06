3986a564740a456b59fbb3772c70e61192c5be32
Uri Goldstein <uri.goldstein@gmail.com>
2017-03-06 15:34:06 +0200

    Integ "test" - Alas, throws exception
    See here: https://github.com/QuickbaseAdmirer/QuickBase-C-Sharp-SDK/issues/8

--- SnapCapital.QuickBase.IntegrationsTests/IntegrationTest.cs
+++ SnapCapital.QuickBase.IntegrationsTests/IntegrationTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Intuit.QuickBase.Client;
+using NUnit.Framework;
 
 namespace SnapCapital.QuickBase.IntegrationsTests
 {
@@ -8,6 +9,10 @@ namespace SnapCapital.QuickBase.IntegrationsTests
         [Test]
         public void TestMethod1()
         {
+            var client = Intuit.QuickBase.Client.QuickBase.Login("uri.goldstein@gmail.com", "Kwyjibo69", "snapcredit.quickbase.com");
+            var app = client.Connect("bmkhmqry9");
+            var table = app.GetTable("bmkhmqrzs");
+
         }
     }
 }
